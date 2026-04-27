fd1820306ede91e6d0ff2c890fedd588642f7475
Artur <pavlechko777@gmail.com>
2026-04-27 11:14:15 +0300

    fix test

--- apps/shop-e2e/src/example.spec.ts
+++ apps/shop-e2e/src/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('redirects to dashboard and shows restaurant details', async ({ page }) => {
   await page.goto('/');
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  await expect(page).toHaveURL(/\/dashboard$/);
+  await expect(page.locator('text=Cart Total: $0')).toBeVisible();
 });

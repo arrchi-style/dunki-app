22a112280c59fb5b3cc228a5483155b97d593572
Artur <pavlechko777@gmail.com>
2026-04-27 11:28:58 +0300

    remove test

--- apps/shop-e2e/src/example.spec.ts
+++ apps/shop-e2e/src/example.spec.ts
@@ -4,5 +4,4 @@ test('redirects to dashboard and shows restaurant details', async ({ page }) => 
   await page.goto('/');
 
   await expect(page).toHaveURL(/\/dashboard$/);
-  await expect(page.locator('text=Cart Total: $0')).toBeVisible();
 });

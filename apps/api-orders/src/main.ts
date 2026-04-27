b0b24dc9cfeb819e7b70601fb255e1b66a295115
Artur <pavlechko777@gmail.com>
2026-04-27 17:06:22 +0300

    add .Net with validation controller

--- apps/api-orders/src/main.ts
+++ apps/api-orders/src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -10,21 +11,38 @@ app.use(express.json());
 
 const orders = [];
 
-app.post('/api/orders', (req, res) => {
+app.post('/api/orders', async (req, res) => {
   const order = req.body;
-  
-  // Додаємо метадані
-  const newOrder = {
-    ...order,
-    id: Date.now().toString(),
-    status: 'pending',
-    createdAt: new Date()
-  };
 
-  orders.push(newOrder);
-  console.log('New Order Received:', newOrder);
-  
-  res.status(201).send(newOrder);
+  try {
+    // Викликаємо наш новий C# сервіс
+    const billingResponse = await axios.post('http://127.0.0.1:5041/api/OrderValidation', {
+      items: order.items,
+      totalFromClient: order.total
+    });
+
+    const { isValid, tax, actualTotal } = billingResponse.data;
+
+    if (!isValid) {
+      console.error(`Security alert! User tried to pay ${order.total} instead of ${actualTotal}`);
+      return res.status(400).json({ error: 'Invalid order total' });
+    }
+
+    // Якщо .NET дав добро — зберігаємо
+    const finalizedOrder = {
+      ...order,
+      tax,
+      id: `ORD-${Date.now()}`,
+      status: 'confirmed'
+    };
+
+    orders.push(finalizedOrder);
+    res.status(201).json(finalizedOrder);
+
+  } catch (error) {
+    console.error('Billing service is unavailable');
+    res.status(503).json({ error: 'Service temporarily unavailable' });
+  }
 });
 
 app.get('/', (req, res) => {
